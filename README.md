a1d43e8943d14680519fc94d0ae4cc489b19f6a4
Rashed Talukder <9218468+rashedtalukder@users.noreply.github.com>
2022-03-21 19:14:04 -0700

    Update to BSP v2

--- README.md
+++ README.md
@@ -1,11 +1,49 @@
-## My Project
+# M5Stack Core2 for AWS IoT EduKit Factory Firmware
 
-TODO: Fill this README out!
+Factory firmware for the M5Stack Core2 for AWS IoT EduKit. Use this repository to restore your device to the original program or to investigate and freely modify.This application was written to be easy to understand and replicate instead of efficiency. View the [API reference](https://edukit.workshop.aws/en/api-reference/v2/index.html) for using the included board support package.
 
-Be sure to:
+## Cloning
+This repo uses [Git Submodules](https://git-scm.com/book/en/v2/Git-Tools-Submodules) to bring in dependent components.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+Note: If you download the ZIP file provided by GitHub UI, you will not get the contents of the submodules. Since the downloaded zip will also not be a git repository, you will not be able to compile the code since that is a toolchain requirement. You must clone the repository using the instructions below.
+
+If using Windows, because this repository and its submodules contain symbolic links, set `core.symlinks` to true with the following command:
+```
+git config --global core.symlinks true
+```
+In addition to this, either enable [Developer Mode](https://docs.microsoft.com/en-us/windows/apps/get-started/enable-your-device-for-development) or, whenever using a git command that writes to the system (e.g. `git pull`, `git clone`, and `git submodule update --init --recursive`), use a console elevated as administrator so that git can properly create symbolic links for this repository. Otherwise, symbolic links will be written as normal files with the symbolic links' paths in them as text. [This](https://blogs.windows.com/windowsdeveloper/2016/12/02/symlinks-windows-10/) gives more explanation.
+
+To clone using HTTPS:
+```
+git clone https://github.com/aws-iot-edukit/Factory_Firmware-Core2_for_AWS.git --recurse-submodules
+```
+Using SSH:
+```
+git clone git@github.com:aws-iot-edukit/Factory_Firmware-Core2_for_AWS.git --recurse-submodules
+```
+
+If you have downloaded the repo without using the `--recurse-submodules` argument, you need to run:
+```
+git submodule update --init --recursive
+```
+
+## Important files/folders
+
+### main/main.c
+
+This is the entry point for your application. Start by investigating and/or modifying this file for your needs.
+
+### components/Core2-for-AWS-IoT-EduKit
+
+This is the location of the [board support package](https://github.com/m5stack/Core2-for-AWS-IoT-EduKit). These include drivers and helper libraries for controlling the on-board peripherals on the device.
+
+### components/esp-cryptoauthlib
+
+This is a [ported cryptoauthlib](https://github.com/espressif/esp-cryptoauthlib) from Espressif. This fork is a specific port for the Core2 for AWS reference hardware. This library enables use of the on-board secure element and is tightly integrated with the ESP-IDF. The name of the folder should not be modified.
+
+### partitions_16mb.csv
+
+This is the partition table recommended for most applications. It provides sufficient file system sizes for storing Wi-Fi credentials, the user application, OTA updates, additional file storage, and storage for SPIFFS in the on-board flash. This utilizes the internal + external flash memory.
 
 ## Security
 
---
+++ main/main.c
@@ -0,0 +1,172 @@
+/*
+ * AWS IoT EduKit - Core2 for AWS IoT EduKit
+ * Factory Firmware v2.2.0
+ * main.c
+ * 
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+
+#include "esp_freertos_hooks.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_vfs_fat.h"
+#include "driver/gpio.h"
+#include "driver/spi_common.h"
+#include "sdmmc_cmd.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+
+#include "core2forAWS.h"
+
+#include "sound.h"
+#include "home.h"
+#include "wifi.h"
+#include "mpu.h"
+#include "mic.h"
+#include "clock.h"
+#include "power.h"
+#include "touch.h"
+#include "led_bar.h"
+#include "crypto.h"
+#include "cta.h"
+
+static const char* TAG = "MAIN";
+
+static void ui_start(void);
+static void tab_event_cb(lv_obj_t* slider, lv_event_t event);
+
+static lv_obj_t* tab_view;
+TaskHandle_t    FFT_handle,
+                led_bar_animation_handle, 
+                led_bar_solid_handle,
+                mic_handle,
+                MPU_handle,
+                touch_handle,
+                wifi_handle;
+
+LV_IMG_DECLARE(powered_by_aws_logo);
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "\n***************************************************\n M5Stack Core2 for AWS IoT EduKit Factory Firmware\n***************************************************");
+
+    esp_log_level_set("gpio", ESP_LOG_NONE);
+    esp_log_level_set("ILI9341", ESP_LOG_NONE);
+
+    core2foraws_init();
+    
+    ui_start();
+}
+
+static void ui_start(void){
+    /* Displays the Powered by AWS logo */
+    xSemaphoreTake(core2foraws_display_semaphore, portMAX_DELAY);   // Takes (blocks) the core2foraws_display_semaphore mutex from being read/written by another task.
+    lv_obj_t* opener_scr = lv_scr_act();   // Create a new LVGL "screen". Screens can be though of as a window.
+    lv_obj_t* aws_img_obj = lv_img_create(opener_scr, NULL);   // Creates an LVGL image object and assigns it as a child of the opener_scr parent screen.
+    lv_img_set_src(aws_img_obj, &powered_by_aws_logo);  // Sets the image object with the image data from the powered_by_aws_logo file which contains hex pixel matrix of the image.
+    lv_obj_align(aws_img_obj, NULL, LV_ALIGN_CENTER, 0, 0); // Aligns the image object to the center of the parent screen.
+    lv_obj_set_style_local_bg_color(opener_scr, LV_OBJ_PART_MAIN, 0, LV_COLOR_WHITE);   // Sets the background color of the screen to white.
+    xSemaphoreGive(core2foraws_display_semaphore);  // Frees the core2foraws_display_semaphore so that another task can use it. In this case, the higher priority guiTask will take it and then read the values to then display.
+
+    /* 
+    You should release the core2foraws_display_semaphore semaphore before calling a blocking function like vTaskDelay because 
+    without doing so, no other task can access it that might need it. This includes the guiTask which
+    writes the objects to the display itself over SPI.
+    */
+
+    vTaskDelay(pdMS_TO_TICKS(1500)); // FreeRTOS scheduler block execution for 1.5 seconds to keep showing the Powered by AWS logo.
+    
+    xTaskCreatePinnedToCore(sound_task, "soundTask", 4096 * 2, NULL, 4, NULL, 1);
+    
+    xSemaphoreTake(core2foraws_display_semaphore, portMAX_DELAY);   // Takes (blocks) the core2foraws_display_semaphore mutex from being read/written by another task.
+    lv_obj_clean(opener_scr);   // Clear the aws_img_obj and remove from memory space. Currently no objects exist on the screen.
+    lv_obj_t* core2forAWS_obj = lv_obj_create(NULL, NULL); // Create an object to draw all with no parent 
+    lv_scr_load_anim(core2forAWS_obj, LV_SCR_LOAD_ANIM_MOVE_LEFT, 400, 0, false);   // Animates the loading of core2forAWS_obj as a slide into view from the left
+    tab_view = lv_tabview_create(core2forAWS_obj, NULL); // Creates the tab view to display different tabs with different hardware features
+    lv_obj_set_event_cb(tab_view, tab_event_cb); // Add a callback for whenever there is an event triggered on the tab_view object (e.g. a left-to-right swipe)
+    lv_tabview_set_btns_pos(tab_view, LV_TABVIEW_TAB_POS_NONE);  // Hide the tab buttons so it looks like a clean screen
+    
+    xSemaphoreGive(core2foraws_display_semaphore);  // Frees the core2foraws_display_semaphore so that another task can use it. In this case, the higher priority guiTask will take it and then read the values to then display.
+
+    /*
+    Below creates all the display layers for the various peripheral tabs. Some of the tabs also starts the concurrent FreeRTOS tasks 
+    that read/write to the peripheral registers and displays the data from that peripheral.
+    */
+    display_home_tab(tab_view);
+    display_clock_tab(tab_view, core2forAWS_obj);
+    display_mpu_tab(tab_view);
+    display_microphone_tab(tab_view);
+    display_LED_bar_tab(tab_view);
+    display_power_tab(tab_view, core2forAWS_obj);
+    display_touch_tab(tab_view);
+    display_crypto_tab(tab_view);
+    display_wifi_tab(tab_view);
+    display_cta_tab(tab_view);
+}
+
+static void tab_event_cb(lv_obj_t* slider, lv_event_t event){
+    if(event == LV_EVENT_VALUE_CHANGED) {
+        lv_tabview_ext_t* ext = (lv_tabview_ext_t*) lv_obj_get_ext_attr(tab_view);
+        const char* tab_name = ext->tab_name_ptr[lv_tabview_get_tab_act(tab_view)];
+        ESP_LOGI(TAG, "Current Active Tab: %s\n", tab_name);
+
+        vTaskSuspend(MPU_handle);
+        vTaskSuspend(mic_handle);
+        vTaskSuspend(FFT_handle);
+        vTaskSuspend(wifi_handle);
+        vTaskSuspend(touch_handle);
+        vTaskSuspend(led_bar_solid_handle);
+        vTaskResume(led_bar_animation_handle);
+        
+        if(strcmp(tab_name, CLOCK_TAB_NAME) == 0)
+            update_roller_time();
+        else if(strcmp(tab_name, MPU_TAB_NAME) == 0)
+            vTaskResume(MPU_handle);
+        else if(strcmp(tab_name, MICROPHONE_TAB_NAME) == 0)
+        {
+            vTaskResume(mic_handle);
+            vTaskResume(FFT_handle);
+        } 
+        else if ( strcmp( tab_name, LED_BAR_TAB_NAME ) == 0 )
+        {
+            vTaskSuspend( led_bar_animation_handle );
+            vTaskResume( led_bar_solid_handle );
+        }
+        else if(strcmp(tab_name, TOUCH_TAB_NAME) == 0)
+        {
+            reset_touch_bg();
+            vTaskResume(touch_handle);
+        }
+        else if(strcmp(tab_name, WIFI_TAB_NAME) == 0)
+            vTaskResume(wifi_handle);
+    }
+}
